d4d75a7b40bb4c07692f25a8007949e4d7b3c14e
carmelc <carmelc@wix.com>
2024-03-21 20:19:06 +0200

    another try

--- .github/wix-cli-publish/run-publish.sh
+++ .github/wix-cli-publish/run-publish.sh
@@ -5,15 +5,12 @@ echo "Running custom publish script"
 echo "WIX_CLI_APP_INFO: $WIX_CLI_APP_INFO"
 echo "WIX_SESSION2: $WIX_SESSION2"
 
-ls -l src wml /mnt
-
+// Use the src and wml folders from the shared volume so it is taken from the repo which runs the docker
 rm -rf src wml
 mkdir src wml
 
 cp -R /mnt/src/* src
 cp -R /mnt/wml/* wml
-
-ls -l src wml
 
 curl -s -X POST "https://editor.wix.com/templates-with-cli-poc/api/poc-app/cli-access" \
             -H "Content-Type: application/json" \
